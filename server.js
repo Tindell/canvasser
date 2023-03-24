4a502bae9d1155a6672f5648136026c3ecbfc821
Tindell Lockett <tindell@lockett.us>
2023-03-24 13:03:41 -0400

    Add body-parser and dotenv dependencies, and implement a Git webhook endpoint for auto-deployment.

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
+
+const crypto = require('crypto');
+const exec = require('child_process').exec;
+
+const bodyParser = require('body-parser');
+const GITHUB_WEBHOOK_SECRET = process.env.GITHUB_WEBHOOK_SECRET || "";
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve canvases from the first directory
 app.get('/load-canvases-1', (req, res) => {
-    console.log("hello");
+  console.log("hello");
   const scriptFolderPath = path.join(__dirname, 'scripts-1');
   readAndSendCanvasScripts(scriptFolderPath, res);
 });
@@ -19,6 +28,49 @@ app.get('/load-canvases-2', (req, res) => {
   const scriptFolderPath = path.join(__dirname, 'scripts-2');
   readAndSendCanvasScripts(scriptFolderPath, res);
 });
+
+app.post('/git-webhook', (req, res) => {
+  updateRepo(req, res);
+});
+
+function updateRepo(req, res) {
+  const payload = JSON.stringify(req.body);
+  const signature = req.header('X-Hub-Signature-256');
+
+  if (!payload || !signature) {
+    res.status(400).send('Invalid request');
+    return;
+  }
+
+  // Verify the payload's signature
+  const hmac = crypto.createHmac('sha256', GITHUB_WEBHOOK_SECRET);
+  const digest = 'sha256=' + hmac.update(payload).digest('hex');
+
+  if (signature !== digest) {
+    res.status(401).send('Unauthorized');
+    return;
+  }
+
+  // Check if the event is a push event to the main branch
+  const event = req.header('X-GitHub-Event');
+  const ref = req.body.ref;
+
+  if (event === 'push' && ref === 'refs/heads/main') {
+    // Update the repository and restart the server
+    exec('git pull && npm install && pm2 restart your-app-name', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+    });
+    res.status(200).send('OK');
+  } else {
+    res.status(200).send('Not a push event to the main branch');
+  }
+};
+
 
 function readAndSendCanvasScripts(scriptFolderPath, res) {
   fs.readdir(scriptFolderPath, (err, files) => {
